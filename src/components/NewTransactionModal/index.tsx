76cf6d83dd7d4254d108521143d80904df956f75
Vinicius <beneditoloura@gmail.com>
2021-09-02 21:59:10 -0300

    feat: create first styles of modal component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import * as Styled from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +11,23 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Styled.Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título" />
+
+        <input placeholder="Valor" type="number" />
+
+        <input placeholder="Categoria" type="number" />
+
+        <button type="submit">Cadastrar</button>
+      </Styled.Container>
     </Modal>
   );
 }
